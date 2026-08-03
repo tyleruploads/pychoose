93652ff4073ba8cb81bc2e776c00458cf85e296d
tyleruploads <tyleruploads@yahoo.com>
2026-08-03 17:31:30 -0400

    First commit: Viable Product

---
+++ README.md
@@ -0,0 +1,49 @@
+# pychoose
+
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue)](https://github.com/tyleruploads/pychoose/blob/main/LICENSE)
+[![GitHub issues](https://img.shields.io/github/issues/tyleruploads/devtkit)](https://github.com/tyleruploads/pychoose/issues)
+[![Python Version](https://img.shields.io/badge/python-3.10%2B-blue)](https://www.python.org/)
+
+[pychoose](https://github.com/tyleruploads/pychoose) is an open-source Python GUI application built with CustomTkinter that removes the friction from everyday decision making.
+The way pychoose does this is by letting the user type in their choices, click a button clearly labeled "Choose" on the bottom of the screen, and receive a cryptographically secure decision via the color of the labels above the text.
+
+
+## Running & Installation
+You can run or install pychoose instantly with [uv](https://docs.astral.sh/uv/getting-started/installation/)
+
+### Running with uv
+```bash
+uvx --from git+https://github.com/tyleruploads/pychoose.git pychoose
+```
+
+### Installing with uv
+```bash
+uv tool install git+https://github.com/tyleruploads/pychoose.git
+```
+
+If you are unable to use [uv](https://docs.astral.sh/uv/getting-started/installation/), you can still manually run the file.
+
+### Manually running
+To manually run the file, you must clone the repository and run the file located at src/pychoose/main.py.
+You must have the customtkinter library accessible by your Python interpreter.
+
+```bash
+git clone https://github.com/tyleruploads/pychoose.git
+cd pychoose
+python3 src/pychoose/main.py
+```
+
+## Contributing
+
+Contributions are what make open-source projects important. All contributions are highly appreciated.
+
+* **Found a bug or issue**: Open an Issue and show the output of the script, the steps to reproduce it, and as much information as possible
+* **Have an idea**: Open an Issue and explain your idea as much as possible, why you think it would be a good addition to the project, and any other important information.
+
+## Security
+
+For information on reporting security vulnerabilities in pychoose, see [SECURITY.md](SECURITY.md)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for more information.
---
+++ src/pychoose/main.py
@@ -0,0 +1,80 @@
+import secrets
+
+import customtkinter as ctk
+
+ctk.set_appearance_mode('black')
+ctk.set_default_color_theme('blue')
+
+
+class ChoiceFrame(ctk.CTkFrame):
+    def __init__(self, master, choice_num, **kwargs):
+        super().__init__(master, **kwargs)
+        self.pack_propagate(False)
+
+        self.label_choice = ctk.CTkLabel(self, text=f"Choice {choice_num}", height=50)
+        self.text_choice = ctk.CTkTextbox(self, height=300)
+
+        self.label_choice.pack(expand=True, fill="both")
+        self.text_choice.pack(expand=True, fill="both")
+
+
+        self.text_choice.bind("<Control-a>", self.select_all)
+        self.text_choice.bind("<Control-A>", self.select_all)
+
+    def select_all(self, event):
+        event.widget.tag_add('sel', '1.0', 'end')
+        return 'break'
+
+
+class App(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.geometry('800x800')
+        self.configure(fg_color="#FF9F50")
+
+        self.label_app_name = ctk.CTkLabel(self, text="pychoose", text_color="white", font=("Arial", 20, "bold"))
+
+        self.frame_choices = ctk.CTkFrame(self)
+        self.frame_choice1 = ChoiceFrame(master=self.frame_choices, fg_color="#5076FF", choice_num=1, width=375, height=350)
+        self.frame_choice2 = ChoiceFrame(master=self.frame_choices, fg_color="#5979FF", choice_num=2, width=375, height=350)
+
+        self.frame_buttons = ctk.CTkFrame(self)
+        self.button_choose = ctk.CTkButton(self.frame_buttons, text="Choose", command=self.choose)
+        self.button_reset = ctk.CTkButton(self.frame_buttons, text="Reset", command=self.reset)
+
+        self.label_app_name.pack(side="top", pady=5)
+        self.frame_buttons.pack(side="bottom", pady=5)
+
+        self.button_choose.pack(side="left", padx=10, pady=5)
+        self.button_reset.pack(side="right", padx=10, pady=5)
+
+        self.frame_choices.pack(expand=True, padx=20, pady=20)
+        self.frame_choice1.pack(side="left", expand=True, padx=5, pady=5)
+        self.frame_choice2.pack(side="right", expand=True, padx=5, pady=5)
+
+        self.bind("<Key>", self.reset_colors)
+
+    def choose(self):
+        choices = [self.frame_choice1, self.frame_choice2]
+        secrets.SystemRandom().shuffle(choices)
+    
+        choice = choices[0]
+        non_choice = choices[1]
+
+        choice.configure(fg_color="green")
+        non_choice.configure(fg_color="red")
+
+    def reset_colors(self, event="<Key>"):
+        self.frame_choice1.configure(fg_color="#5076FF")
+        self.frame_choice2.configure(fg_color="#5979FF")
+
+    def reset(self):
+        self.reset_colors(self)
+
+def main():
+    app = App()
+    app.mainloop()
+    
+
+if __name__ == "__main__":
+    main()
